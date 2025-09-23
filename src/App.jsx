1eee82ffa11fdd88e9ae638edec7eceb7b186f14
Sakhi Akram <sakhiakram.github@outlook.com>
2025-09-22 23:36:42 -0700

    learnt and used props
    keeping code as explicit as possible

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ const Pizza = () => {
         "div",
         {},
        [
-        React.createElement("h1", {}, "The Pepperoni Pizza" ),
-        React.createElement("p", {}, "ham, cheese" )
+        React.createElement("h1", {}, props.name ),
+        React.createElement("p", {}, props.desc )
        ]
     ) 
     
@@ -19,11 +19,22 @@ const App = () => {
             "h1", {}, "Padre Gino's"
         ),
 
-        React.createElement(Pizza),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
-        React.createElement(Pizza)
+        React.createElement(Pizza, {
+            name: "The Pepporoni Pizza",
+            desc: "hmmm"
+        }),
+            React.createElement(Pizza, {
+            name: "Italian Pizza",
+            desc: "hmmm"
+        }),
+            React.createElement(Pizza, {
+            name: "Happy Pizza",
+            desc: "hmmm"
+        }),
+            React.createElement(Pizza, {
+            name: "Sad Pizza",
+            desc: "hmmm"
+        })
     ]
     )   
 }
@@ -42,6 +53,11 @@ By calling it once we stamp it once in the page
 
 Reusable Components, on line 1 we declared and defined another component called Pizza.
 This component is then used in the App component many times which result in it displaying many times. 
+
+This is now been written on my third comment.
+
+I am now using props to make my components more reusable and useful and this saves us alot of time. As you can read the code and understand most of the code.
+To explain some concepts I am using explicit code. Use version control to understand this code better.
 
 
 */
