78d7f537f589c12554a53d32da41550ed16348ea
Sakhi Akram <sakhiakram.github@outlook.com>
2025-09-25 08:53:50 +0500

    small fix to pizza component

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,7 +1,7 @@
 const Pizza = (props) => {
     return (
         <div className="pizza">
-        <h2>{props.name}</h2>
+        <h1>{props.name}</h1>
         <p>{props.desc}</p>
         <img src={props.image} alt={props.name} />
         </div>
