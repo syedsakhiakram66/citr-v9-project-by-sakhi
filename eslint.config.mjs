a03132a09fee480426ae480709214cb7ba163e5f
Sakhi Akram <sakhiakram.github@outlook.com>
2025-09-25 07:59:33 +0500

    eslint configuration for react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,22 @@
 import js from "@eslint/js";
 import globals from "globals";
-// always put prettier last
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js"],
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
@@ -15,6 +24,10 @@ export default [
           jsx: true,
         },
       },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
     },
   },
   prettier,
