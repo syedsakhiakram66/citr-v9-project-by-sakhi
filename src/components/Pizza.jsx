b72f5edbd5538f62761dee12c05dc12deff26d60
Sakhi Akram <sakhiakram.github@outlook.com>
2025-09-28 10:50:41 +0500

    desc changed to description

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -2,7 +2,7 @@ const Pizza = (props) => {
     return (
         <div className="pizza">
         <h1>{props.name}</h1>
-        <p>{props.desc}</p>
+        <p>{props.description}</p>
         <img src={props.image} alt={props.name} />
         </div>
     )
