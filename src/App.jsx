6c50852cab62490327e42b49bbbc92c02564824b
Sakhi Akram <sakhiakram.github@outlook.com>
2025-10-02 10:44:42 +0500

    using contexts and setting up CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import * as ReactDOM from "react-dom/client";
+import { StrictMode, useState } from "react";
 import Order from "./components/Order";  
 import PizzaOfTheDay from "./components/PizzaOfTheDay";
 import Header from "./components/Header";
+import { CartContext } from "./components/contexts";
    
 
 const App = () => {
+  const cartHook = useState([]);
   return (
     <div>
+      <StrictMode>
+    <CartContext.Provider value={cartHook}>
       <Header />
       <Order />
       <PizzaOfTheDay />
+    </CartContext.Provider>
+      </StrictMode>
     </div>
   );
 };
