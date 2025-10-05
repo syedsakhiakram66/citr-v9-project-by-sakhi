354cbd986d0c4dca1927346affb60bbb8c633652
Sakhi Akram <sakhiakram.github@outlook.com>
2025-10-05 11:17:32 +0500

    used QueryClient in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import * as ReactDOM from "react-dom/client";
 import { StrictMode} from "react";
 import {routeTree} from "./routeTree.gen"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <div>
       <StrictMode>
+        <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        </QueryClientProvider>
       </StrictMode>
     </div>
   );
