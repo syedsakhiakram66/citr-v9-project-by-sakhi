d84bfbe9ea41431c5bb96465164eedf0f763c5e4
Sakhi Akram <sakhiakram.github@outlook.com>
2025-10-12 12:28:58 +0500

    made another test for pizza.jsx

--- src/__tests__/Pizza.test.jsx
+++ src/__tests__/Pizza.test.jsx
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { render, cleanup } from "@testing-library/react";
+import { afterEach, expect, test } from "vitest";
 import Pizza from "../components/Pizza";
+
+afterEach(cleanup);
 
 test("alt text renders on image", async () => {
   const name = "My Favorite Pizza";
@@ -12,4 +14,12 @@ test("alt text renders on image", async () => {
   const img = screen.getByRole("img");
   expect(img.src).toBe(src);
   expect(img.alt).toBe(name);
+});
+
+test("to have default image if none is provided", async () => {
+  const screen = render(
+    <Pizza name="My Favorite Pizza" description="super cool pizza" />
+  );
+  const img = screen.getByRole("img");
+  expect(img.src).not.toBe(""); 
 });
